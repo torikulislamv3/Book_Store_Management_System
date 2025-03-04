93af5eca39e2d836a850b8bf1953eecfb8a2a1e2
MD TORIKUL ISLAM <tamim.officialinfo7@gmail.com>
2025-03-04 23:58:08 +0600

    remove book file added

---
+++ remove_book.py
@@ -0,0 +1,13 @@
+from book_data import save_books
+
+def remove_book(books):
+    book_id = input("Enter ISBN/Book ID to remove: ")
+    
+    for book in books:
+        if book['book_id'] == book_id:
+            books.remove(book)
+            save_books(books)
+            print("Book removed successfully!")
+            return
+    
+    print("Book not found!")
